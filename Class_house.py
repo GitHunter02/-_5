64583b0d9ab04bf2eadaa81c9f2b4578bd4551a5
GitHunter02 <timur.ruslanovich25@gmail.com>
2024-12-24 00:34:23 +0500

    Add files via upload

--- Class_house.py
+++ Class_house.py
@@ -7,11 +7,9 @@ class House:
         if new_floor < 1 or new_floor > self.number_of_floor:
             print('Данный этаж не предусмотрен в планировке здания')
         else:
-            for i in range(1, new_floor + 1):
-                if i == new_floor:
-                    print(f"Вы доехали до {new_floor} этажа")
-                else:
-                    print(f"Проехали {i} этаж")
+            for i in range(1, new_floor):
+                print(f"Проехали {i} этаж")
+            print(f"Вы доехали до {new_floor} этажа")
 
     def __len__(self):
         return self.number_of_floor
@@ -23,10 +21,11 @@ class House:
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 
+
 # __str__
 print(h1)
 print(h2)
 
-# __len__
+# # __len__
 print(len(h1))
 print(len(h2))
