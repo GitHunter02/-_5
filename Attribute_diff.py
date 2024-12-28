624943ce28011bd6a7fc388178374e049dc339bd
GitHunter02 <timur.ruslanovich25@gmail.com>
2024-12-28 16:07:35 +0500

    Add files via upload

---
+++ Attribute_diff.py
@@ -0,0 +1,70 @@
+class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        instance = super().__new__(cls)
+        cls.houses_history.append(args[0])
+        return instance
+
+    def __init__(self, name, number_of_floor):
+        self.name = name
+        self.number_of_floor = number_of_floor
+
+    def go_to(self, new_floor):
+        if new_floor < 1 or new_floor > self.number_of_floor:
+            print('Данный этаж не предусмотрен в планировке здания')
+        else:
+            for i in range(1, new_floor):
+                print(f"Проехали {i} этаж")
+            print(f"Вы доехали до {new_floor} этажа")
+
+    def __len__(self):
+        return self.number_of_floor
+
+    def __eq__(self, other):
+        return self.number_of_floor == other.number_of_floor
+
+    def __lt__(self, other):
+        return self.number_of_floor < other.number_of_floor
+
+    def __le__(self, other):
+        return self.number_of_floor <= other.number_of_floor
+
+    def __gt__(self, other):
+        return self.number_of_floor > other.number_of_floor
+
+    def __ge__(self, other):
+        return self.number_of_floor >= other.number_of_floor
+
+    def __ne__(self, other):
+        return self.number_of_floor != other.number_of_floor
+
+    def __add__(self, value):
+        self.number_of_floor += value
+        return self
+
+    def __radd__(self, value):
+        return self + value
+
+    def __iadd__(self, value):
+        self.number_of_floor += value
+        return self
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floor}"
+
+    def __del__(self):
+        print(f"{self.name} снесён, но он останется в истории")
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
