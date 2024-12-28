3cde4ee1c8dc153d50714ef64aca9fa5f988b07d
GitHunter02 <timur.ruslanovich25@gmail.com>
2024-12-28 14:14:47 +0500

    Add files via upload

--- Class_house.py
+++ Class_house.py
@@ -14,6 +14,35 @@ class House:
     def __len__(self):
         return self.number_of_floor
 
+    def __eq__(self, other):
+        return self.number_of_floor == other.number_of_floor
+
+    def __lt__(self, other):
+        return self.number_of_floor < other.number_of_floor
+
+    def __le__(self, other):
+        return self.number_of_floor <= other.number_of_floor
+
+    def __gt__(self, other):
+        return self.number_of_floor > other.number_of_floor
+
+    def __ge__(self, other):
+        return self.number_of_floor >= other.number_of_floor
+
+    def __ne__(self, other):
+        return self.number_of_floor != other.number_of_floor
+
+    def __add__(self, value):
+        self.number_of_floor += value
+        return self
+
+    def __radd__(self, value):
+        return self + value
+
+    def __iadd__(self, value):
+        self.number_of_floor += value
+        return self
+
     def __str__(self):
         return f"Название: {self.name}, кол-во этажей: {self.number_of_floor}"
 
@@ -25,6 +54,37 @@ h2 = House('ЖК Акация', 20)
 print(h1)
 print(h2)
 
-# # __len__
+#  __len__
 print(len(h1))
 print(len(h2))
+
+# __eq__
+print(h1 == h2)
+
+# __add__
+h1 = h1 + 10
+print(h1)
+print(h1 == h2)
+
+# __iadd__
+h1 += 10
+print(h1)
+
+# __radd__
+h2 = 10 +h2
+print(h2)
+
+# __gt__
+print(h1 > h2)
+
+# __ge__
+print(h1 >= h2)
+
+# __lt__
+print(h1 < h2)
+
+# __le__
+print(h1 <= h2)
+
+# __ne__
+print(h1 != h2)
